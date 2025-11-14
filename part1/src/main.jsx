aa14830ccd5f7a3fea62c8c2f994173f9670af84
Krzysztof Nurek <nurekk@tlimc.local>
2025-11-14 14:08:24 +0100

    Ex 1.1 & 1.2

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
+
+import App1_2 from './App1_2'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App1_2 />
   </StrictMode>,
 )
+
+//createRoot(document.getElementById('root')).render(
+//  <StrictMode>
+//    <App />
+//  </StrictMode>,
+//)
+
+
+
+
