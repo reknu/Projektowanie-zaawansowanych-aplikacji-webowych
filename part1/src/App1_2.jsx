1a5cd3fbc9b6cab196f02a2469d2f94df425f4eb
Krzysztof Nurek <nurekk@tlimc.local>
2026-02-26 14:24:45 +0100

    -

---
+++ part1/src/App1_2.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+// Nowy komponent — Part
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Zadanie = () => {
+    return(
+        <div>
+            <h2>
+            Zadanie
+        </h2>
+        <p>
+            Krzysztof Nurek
+        </p> 
+        </div>
+       
+    )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App1_2 = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Zadanie/>
+    </div>
+  )
+}
+
+export default App1_2
